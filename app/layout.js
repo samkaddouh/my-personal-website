7b48cd9a8d8e45e50185ae12c18918777087068b
samkadouh1 <30523207+samkaddouh@users.noreply.github.com>
2025-02-24 11:09:14 -0500

    edited favicon. safari rendering needs work

--- app/layout.js
+++ app/layout.js
@@ -3,9 +3,8 @@ import { Footer } from "./components/footer"
 import { ThemeProvider } from "./components/theme-provider"
 import { Orbitron } from "next/font/google"
 import "./globals.css"
-import { Analytics } from '@vercel/analytics/next'
-import { SpeedInsights } from '@vercel/speed-insights/next'
-
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const orbitron = Orbitron({
   subsets: ["latin"],
@@ -17,17 +16,34 @@ const orbitron = Orbitron({
 export const metadata = {
   title: "Sam",
   description: "Portfolio of Sam Kadouh",
+  icons: {
+    shortcut: ["/favicon.ico"],
+    apple: [{ url: "/apple-touch-icon.png" }, { url: "/favicon.ico" }],
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      </head>
       <body className={`${orbitron.variable} bg-background text-foreground min-h-screen flex flex-col`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <MainNavbar />
-          <main className="flex-grow">{children}
-          <Analytics />
-          <SpeedInsights/>
+          <main className="flex-grow">
+            {children}
+            <Analytics />
+            <SpeedInsights />
           </main>
           <Footer />
         </ThemeProvider>
